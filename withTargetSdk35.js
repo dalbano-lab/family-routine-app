64576d4270fecc7b86318d3d9f5d83745fa0d8e2
Daiane.Albano <daiarb@gmail.com>
2026-06-07 20:19:02 -0300

    plugin targetSdk 35 corrigido

--- withTargetSdk35.js
+++ withTargetSdk35.js
@@ -1,10 +1,34 @@
-const { withAppBuildGradle } = require('@expo/config-plugins');
+const { withAppBuildGradle, withProjectBuildGradle } = require('@expo/config-plugins');
 
-module.exports = function withTargetSdk35(config) {
-  return withAppBuildGradle(config, function(config) {
-    config.modResults.contents = config.modResults.contents
-      .replace(/targetSdkVersion\s*=?\s*\d+/g, 'targetSdkVersion = 35')
-      .replace(/compileSdkVersion\s*=?\s*\d+/g, 'compileSdkVersion = 35');
+const withTargetSdk35 = (config) => {
+  config = withAppBuildGradle(config, (config) => {
+    let contents = config.modResults.contents;
+    
+    // Substitui targetSdkVersion
+    contents = contents.replace(
+      /targetSdkVersion\s*=?\s*Integer\.parseInt\(findProperty\(['"]android\.targetSdkVersion['"]\)\.toString\(\)\)/,
+      'targetSdkVersion 35'
+    );
+    contents = contents.replace(/targetSdkVersion\s+\d+/g, 'targetSdkVersion 35');
+    contents = contents.replace(/targetSdkVersion\s*=\s*\d+/g, 'targetSdkVersion = 35');
+    
+    // Substitui compileSdkVersion
+    contents = contents.replace(/compileSdkVersion\s+\d+/g, 'compileSdkVersion 35');
+    contents = contents.replace(/compileSdkVersion\s*=\s*\d+/g, 'compileSdkVersion = 35');
+    
+    config.modResults.contents = contents;
     return config;
   });
+
+  config = withProjectBuildGradle(config, (config) => {
+    let contents = config.modResults.contents;
+    contents = contents.replace(/targetSdkVersion\s*=\s*\d+/g, 'targetSdkVersion = 35');
+    contents = contents.replace(/compileSdkVersion\s*=\s*\d+/g, 'compileSdkVersion = 35');
+    config.modResults.contents = contents;
+    return config;
+  });
+
+  return config;
 };
+
+module.exports = withTargetSdk35;
